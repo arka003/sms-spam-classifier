611aa41fcbc2724444a40fba22026e42738fc34e
Arka Mandal <mandal.arka003@gmail.com>
2024-08-12 14:56:37 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ nltk.data.path.append(nltk_data_dir)
 nltk.download('punkt', download_dir=nltk_data_dir)
 nltk.download('punkt_tab', download_dir=nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
-nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_dir)
-nltk.download('wordnet', download_dir=nltk_data_dir)
-nltk.download('omw-1.4', download_dir=nltk_data_dir)
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import os
