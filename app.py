a89837ff731ff1c4d4a8414dc322060e48cabe58
Arka Mandal <91402584+arka003@users.noreply.github.com>
2024-08-12 14:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 import os
 
-nltk_data_path = os.path.join(os.getcwd(), '~/nltk_data')
-nltk.data.path.append(nltk_data_path)
-nltk.download('punkt', download_dir=nltk_data_path)
+nltk.data.path.append('/opt/render/nltk_data')
+
 ps = PorterStemmer()
 
 
